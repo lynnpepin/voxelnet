c8339772b79cdf5e40f82df00e5189a9d97b8b6d
Lynn Pepin <lynn.pepin@uconn.edu>
2021-11-19 12:46:37 -0500

    Init layers

---
+++ layers.py
@@ -0,0 +1,15 @@
+'''layers.py
+
+Contains the novel layers defined in VoxelNet.
+'''
+
+import tensorflow as tf
+from tensorflow import keras
+
+class VoxelPartitionLayer(keras.layers.Layer):
+    def __init__(self):
+        pass
+
+    def call(self, inputs):
+        pass
+        
